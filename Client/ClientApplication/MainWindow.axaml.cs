689c5c8360ce8d52ed042272677e1d87c4a6430b
Dan Cojocaru <dancojocaru2010@hotmail.com>
2022-05-28 00:18:50 +0300

    Added Render Andorra City button

--- Client/ClientApplication/MainWindow.axaml.cs
+++ Client/ClientApplication/MainWindow.axaml.cs
@@ -17,10 +17,12 @@ public partial class MainWindow : Window
         InitializeComponent();
         DataContext = this;
         Model.Data = "Render";
-    }
+		RenderCityModel.Data = "Render Andorra City";
+	}
 
     // Model used for the button text
     private DataModel Model { get; } = new DataModel();
+    private DataModel RenderCityModel { get; } = new DataModel();
     // Model used for the list of items
     private ObservableCollection<MapTile> Items { get; } = new ObservableCollection<MapTile>();
 
@@ -41,6 +43,22 @@ public partial class MainWindow : Window
         {
             Console.Error.WriteLine(ex.Message);
         }
+    }
+
+    private void OnRenderCityPressed(object? sender, RoutedEventArgs eventArgs) {
+		try
+		{
+			var response = _httpClient.GetAsync("http://localhost:8080/render?minLon=1.52&minLat=42.5022817&maxLon=1.5285380&maxLat=42.5103326&size=2000").Result;
+			if (response.IsSuccessStatusCode)
+			{
+				Items.Add(new MapTile(response.Content.ReadAsByteArrayAsync().Result, 2000));
+			}
+
+		}
+		catch (Exception ex)
+		{
+			Console.Error.WriteLine(ex.Message);
+		}
     }
 
     private class ServiceResponse
